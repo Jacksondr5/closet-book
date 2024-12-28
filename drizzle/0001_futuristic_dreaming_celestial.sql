add40d0d8a5701c4a3907f63ac78fcd7919dd9eb
Jackson Miller <jackson@jackson.codes>
2024-12-28 16:32:40 -0500

    reset db stuff

--- drizzle/0001_futuristic_dreaming_celestial.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "closet-book_garment" 
-  ALTER COLUMN "style_season" TYPE season[] USING ARRAY[style_season]::season[];--> statement-breakpoint
-ALTER TABLE "closet-book_garment" 
-  ALTER COLUMN "weather_season" TYPE season[] USING ARRAY[weather_season]::season[];
